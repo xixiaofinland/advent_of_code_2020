9984623f295945dfa497ad8bca5643f1e8ed6af7
xi xiao <tdxiaoxi2@gmail.com>
2025-05-01 10:04:06 +0300

    +

--- src/day1.rs
+++ src/day1.rs
@@ -1,3 +1,4 @@
+use core::num;
 use std::{
     collections::HashSet,
     fs::File,
@@ -26,6 +27,34 @@ pub fn solve_day1a() -> AoCResult<Option<usize>> {
             }
         }
         seen.insert(num);
+    }
+
+    Ok(None)
+}
+
+pub fn solve_day1b() -> AoCResult<Option<usize>> {
+    let file = File::open("data/input_day1a.txt")?;
+    let reader = BufReader::new(file);
+
+    let numbers: Vec<usize> = reader
+        .lines()
+        .filter_map(|line| line.ok()?.trim().parse().ok())
+        .collect();
+
+    for (i, &a) in numbers.iter().enumerate() {
+        let target: usize = 2020 - a;
+
+        let mut seen = HashSet::new();
+        for &b in &numbers[i + 1..] {
+            let complement = target.checked_sub(b);
+            if let Some(c) = complement {
+                let has_seen = seen.contains(&c);
+                if has_seen {
+                    return Ok(Some(a * b * c));
+                }
+            }
+            seen.insert(b);
+        }
     }
 
     Ok(None)
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use aoc_2020::day1::solve_day1a;
+use aoc_2020::day1::*;
 
 pub fn main() {
-    match solve_day1a() {
+    // match solve_day1a() {
+    match solve_day1b() {
         Ok(Some(result)) => {
             println!("Result: {}", result);
         }
