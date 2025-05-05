650c74b533b36c6a04e1c92c92ebe402c52a3b20
xi xiao <tdxiaoxi2@gmail.com>
2025-05-05 20:44:24 +0300

    +

--- src/day5.rs
+++ src/day5.rs
@@ -19,32 +19,42 @@ pub fn solve_day5a() -> AoCResult<usize> {
 }
 
 fn parse_ticket(pass: &str) -> usize {
-    let (a, b) = pass.split_at(pass.len().saturating_sub(3));
-
-    let mut seat_range = (0, 127);
-
-    for c in a.chars() {
-        seat_range = match c {
-            'F' => (
-                seat_range.0,
-                seat_range.0 + (seat_range.1 - seat_range.0) / 2,
-            ),
-            'B' => (
-                seat_range.0 + (seat_range.1 - seat_range.0 + 1) / 2,
-                seat_range.1,
-            ),
-            _ => panic!(),
-        };
-    }
-
-    let mut column = (0, 7);
-    for d in b.chars() {
-        column = match d {
-            'L' => (column.0, column.0 + (column.1 - column.0) / 2),
-            'R' => (column.0 + (column.1 - column.0 + 1) / 2, column.1),
-            _ => panic!(),
-        };
-    }
-
-    seat_range.0 * 8 + column.0
+    pass.chars().fold(0, |acc, c| {
+        acc << 1 | match c {
+            'B' | 'R' => 1,
+            'F' | 'L' => 0,
+            _ => panic!("Invalid character in boarding pass"),
+        }
+    })
 }
+
+// fn parse_ticket(pass: &str) -> usize {
+//     let (a, b) = pass.split_at(pass.len().saturating_sub(3));
+//
+//     let mut seat_range = (0, 127);
+//
+//     for c in a.chars() {
+//         seat_range = match c {
+//             'F' => (
+//                 seat_range.0,
+//                 seat_range.0 + (seat_range.1 - seat_range.0) / 2,
+//             ),
+//             'B' => (
+//                 seat_range.0 + (seat_range.1 - seat_range.0 + 1) / 2,
+//                 seat_range.1,
+//             ),
+//             _ => panic!(),
+//         };
+//     }
+//
+//     let mut column = (0, 7);
+//     for d in b.chars() {
+//         column = match d {
+//             'L' => (column.0, column.0 + (column.1 - column.0) / 2),
+//             'R' => (column.0 + (column.1 - column.0 + 1) / 2, column.1),
+//             _ => panic!(),
+//         };
+//     }
+//
+//     seat_range.0 * 8 + column.0
+// }
