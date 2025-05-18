4303373a18e8b6cec5b27ef8b99d02eeba506bc1
xi xiao <tdxiaoxi2@gmail.com>
2025-05-18 16:53:26 +0300

    +

--- src/day12a.rs
+++ src/day12a.rs
@@ -1,12 +1,47 @@
 use crate::AoCResult;
 use std::{fs::File, io::{BufRead, BufReader}};
 
+pub struct Instruct{
+    direct: Direct,
+    num: usize,
+}
+
+enum Direct{
+    N,
+    S,
+    W,
+    E,
+}
+
+// F10
+// N3
+// F7
+// R90
+// F11
+
+impl From<String> for Instruct {
+    fn from(s: String) -> Self {
+        let (direct, num) = s.split_at(1);
+        let direct = match direct {
+            "N" => Direct::N,
+            "S" => Direct::S,
+            "W" => Direct::W,
+            "E" => Direct::E,
+            _ => panic!("Invalid direction"),
+        };
+        let num = num.parse::<usize>().expect("Invalid number");
+        Instruct { direct, num }
+    }
+}
+
 pub fn solve_day12a() -> AoCResult<usize> {
     let file = File::open("data/input_day12a_simple.txt")?;
     let reader = BufReader::new(file);
-    for line in reader.lines(){
-        let line = line?;
-        println!("{line}");
-    }
+
+    let instructions = reader.lines().map(|line_result| -> AoCResult<Instruct>{
+        let line = line_result?;
+
+    })
     Ok(0)
 }
+
