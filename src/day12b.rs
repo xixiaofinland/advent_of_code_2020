c50d2087664c9226dde51a89e048a7219a2cb84c
xi xiao <tdxiaoxi2@gmail.com>
2025-05-19 20:55:13 +0300

    +

--- src/day12b.rs
+++ src/day12b.rs
@@ -33,28 +33,80 @@ impl From<&str> for Action {
 }
 
 #[derive(Debug)]
-struct State {
-    dir: i32, // 0=N, 1=E, 2=S, 3=W
+struct WaypointState {
+    x: i32, // "+" east, "-" west
+    y: i32, // "+" north, "-" south
+}
+
+#[derive(Debug)]
+struct ShipState {
     x: i32,
     y: i32,
 }
 
+#[derive(Debug)]
+struct State {
+    waypoint: WaypointState,
+    ship: ShipState,
+}
+
 impl State {
+    fn new() -> Self {
+        Self {
+            waypoint: WaypointState { x: 10, y: 1 },
+            ship: ShipState { x: 0, y: 0 },
+        }
+    }
+
     fn apply(&mut self, action: Action) {
         match action {
-            Action::N(n) => self.y -= n,
-            Action::S(n) => self.y += n,
-            Action::E(n) => self.x += n,
-            Action::W(n) => self.x -= n,
-            Action::L(n) => self.dir = (self.dir + 4 - n / 90) % 4,
-            Action::R(n) => self.dir = (self.dir + n / 90) % 4,
-            Action::F(n) => match self.dir {
-                0 => self.y -= n,
-                1 => self.x += n,
-                2 => self.y += n,
-                3 => self.x -= n,
-                _ => unreachable!(),
-            },
+            Action::N(n) => self.waypoint.y += n,
+            Action::S(n) => self.waypoint.y -= n,
+
+            Action::E(n) => self.waypoint.x += n,
+            Action::W(n) => self.waypoint.x -= n,
+
+            Action::R(n) => {
+                let (x, y) = (self.waypoint.x, self.waypoint.y);
+                match n % 360 {
+                    90 => {
+                        self.waypoint.x = y;
+                        self.waypoint.y = -x;
+                    }
+                    180 => {
+                        self.waypoint.x = -x;
+                        self.waypoint.y = -y;
+                    }
+                    270 => {
+                        self.waypoint.x = -y;
+                        self.waypoint.y = x;
+                    }
+                    _ => panic!("Invalid R rotation: {}", n),
+                }
+            }
+            Action::L(n) => {
+                let (x, y) = (self.waypoint.x, self.waypoint.y);
+                match n % 360 {
+                    90 => {
+                        self.waypoint.x = -y;
+                        self.waypoint.y = x;
+                    }
+                    180 => {
+                        self.waypoint.x = -x;
+                        self.waypoint.y = -y;
+                    }
+                    270 => {
+                        self.waypoint.x = y;
+                        self.waypoint.y = -x;
+                    }
+                    _ => panic!("Invalid L rotation: {}", n),
+                }
+            }
+
+            Action::F(n) => {
+                self.ship.x += self.waypoint.x * n;
+                self.ship.y += self.waypoint.y * n;
+            }
         }
     }
 }
@@ -68,11 +120,14 @@ pub fn solve_day12b() -> AoCResult<usize> {
         .map(|line| -> AoCResult<_> { Ok(Action::from(line?.as_str())) })
         .collect::<Result<Vec<_>, _>>()?;
 
-    let mut state = State { dir: 1, x: 0, y: 0 };
+    let mut state = State::new();
 
     for action in actions {
         state.apply(action);
     }
+    eprintln!("gopro[372]: day12b.rs:126: state={:#?}", state);
 
-    Ok((state.x.abs() + state.y.abs()).try_into().unwrap())
+    Ok((state.ship.x.abs() + state.ship.y.abs())
+        .try_into()
+        .unwrap())
 }
