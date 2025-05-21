9ab98ba5932bfb31c0f199146973824653cfefe2
xi xiao <tdxiaoxi2@gmail.com>
2025-05-21 20:36:55 +0300

    +

--- src/day14a.rs
+++ src/day14a.rs
@@ -1,17 +1,34 @@
 use crate::AoCResult;
 use std::{
+    collections::HashMap,
     fs::File,
     io::{BufRead, BufReader},
 };
 
 pub fn solve_day14a() -> AoCResult<usize> {
-    let file = File::open("data/input_day8a_simple.txt")?;
+    let file = File::open("data/input_day14a.txt")?;
     let reader = BufReader::new(file);
+
+    let mut memory: HashMap<u64, u64> = HashMap::new();
+    let mut current_mask = String::new();
+
     for line in reader.lines() {
         let line = line?;
-        println!("{line}");
+        if line.starts_with("mask") {
+            current_mask = line.split(" = ").nth(1).unwrap().to_string();
+        } else if line.starts_with("mem") {
+            let left_bracket = line.find('[').unwrap();
+            let right_bracket = line.find(']').unwrap();
+            let addr: u64 = line[left_bracket + 1..right_bracket].parse().unwrap();
+            let value_str = line.split(" = ").nth(1).unwrap();
+            let value: u64 = value_str.parse().unwrap();
+            let masked_value = apply_mask(&current_mask, value);
+            memory.insert(addr, masked_value);
+        }
     }
-    Ok(0)
+
+    let sum: u64 = memory.values().sum();
+    Ok(sum as usize)
 }
 
 fn apply_mask(mask: &str, value: u64) -> u64 {
