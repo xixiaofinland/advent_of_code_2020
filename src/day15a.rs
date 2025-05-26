6c42b3400f21a26cb8dc931bc3fc2eb676ef9bdd
xi xiao <tdxiaoxi2@gmail.com>
2025-05-26 20:57:15 +0300

    +

--- src/day15a.rs
+++ src/day15a.rs
@@ -2,7 +2,29 @@ use crate::AoCResult;
 use std::collections::HashMap;
 
 pub fn solve_day15a() -> AoCResult<usize> {
-    let mut nums: HashMap<usize, usize> = HashMap::new();
+    let starting_numbers = [0, 20, 7, 16, 1, 18, 15];
+    let mut last_seen = HashMap::new();
+
+    let mut iter = starting_numbers.iter().copied().enumerate();
+    for (turn, num) in iter.by_ref().take(starting_numbers.len() - 1) {
+        last_seen.insert(num, turn + 1);
+    }
+
+    let (_, mut current) = iter.next().unwrap(); // last starting number
+
+    for turn in starting_numbers.len()..2020 {
+        let next = match last_seen.insert(current, turn) {
+            Some(prev_turn) => turn - prev_turn,
+            None => 0,
+        };
+        current = next;
+    }
+
+    Ok(current)
+}
+
+pub fn solve_day15a_my() -> AoCResult<usize> {
+    let mut nums = HashMap::new();
 
     let init_inputs = [0, 20, 7, 16, 1, 18];
 
