01e1615de49c5c2db3fc57ffec85cb8fce2e1472
xi xiao <tdxiaoxi2@gmail.com>
2025-05-27 21:34:39 +0300

    +

--- src/day16a.rs
+++ src/day16a.rs
@@ -1,12 +1,31 @@
+use std::fs;
+
 use crate::AoCResult;
-use std::{fs::File, io::{BufRead, BufReader}};
 
 pub fn solve_day16a() -> AoCResult<usize> {
-    let file = File::open("data/input_day8a_simple.txt")?;
-    let reader = BufReader::new(file);
-    for line in reader.lines(){
-        let line = line?;
-        println!("{line}");
+    let file = fs::read_to_string("data/input_day16a_simple.txt")?;
+    let mut chunks = file.split("\n\n");
+
+    for line in chunks.next().unwrap().split("\n") {
+        let mut nums = line
+            .split(": ")
+            .skip(1)
+            .next()
+            .unwrap()
+            .split(" or ")
+            .skip(1)
+            .next()
+            .unwrap()
+            .split("-");
+        println!("{}", nums.next().unwrap());
+        println!("{}", nums.next().unwrap());
+        println!();
     }
+
+    for line in chunks.skip(1).next().unwrap().split("\n") {
+        let nearby_nums: Vec<_> = line.split(",").collect();
+        eprintln!("gopro[388]: day16a.rs:26: nearby_nums={:#?}", nearby_nums);
+    }
+
     Ok(0)
 }
