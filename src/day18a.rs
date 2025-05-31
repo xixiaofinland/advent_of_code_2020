cf06529c9caf8174beb498dc00a72879940d0d29
xi xiao <tdxiaoxi2@gmail.com>
2025-05-31 20:17:14 +0300

    +

--- src/day18a.rs
+++ src/day18a.rs
@@ -1,31 +1,67 @@
 use regex::Regex;
-
-use crate::AoCResult;
 use std::{
     fs::File,
     io::{BufRead, BufReader},
 };
 
+use crate::AoCResult;
+
 pub fn solve_day18a() -> AoCResult<usize> {
-    let file = File::open("data/input_day18a_simple.txt")?;
+    let file = File::open("data/input_day18a.txt")?;
     let reader = BufReader::new(file);
 
-    let re = Regex::new(r"\(|\)|\w+").unwrap();
+    let re = Regex::new(r"\d+|[()+*]")?;
 
-    let tokens: Vec<Vec<_>> = reader
+    let sum = reader
         .lines()
         .map(|line| -> AoCResult<_> {
-            line?
-                .split_whitespace()
-                .try_fold(Vec::new(), |mut acc, element| -> AoCResult<_> {
-                    acc.extend(re.find_iter(element).map(|m| m.as_str().to_string()));
-                    Ok(acc)
-                })
+            let line = line?;
+            let tokens: Vec<String> = re
+                .find_iter(&line)
+                .map(|m| m.as_str().to_string())
+                .collect();
+            Ok(parse_expr(&mut tokens.into_iter()))
         })
-        .collect::<Result<_, _>>()?;
+        .collect::<Result<Vec<_>, _>>()?
+        .into_iter()
+        .sum();
 
-    eprintln!("gopro[408]: day18a.rs:10: tokens={:#?}", tokens);
-    Ok(0)
+    Ok(sum)
+}
+
+fn parse_expr<I>(tokens: &mut I) -> usize
+where
+    I: Iterator<Item = String>,
+{
+    let mut acc = parse_operand(tokens);
+
+    while let Some(op) = tokens.next() {
+        if op == ")" {
+            break;
+        }
+
+        let rhs = parse_operand(tokens);
+
+        acc = match op.as_str() {
+            "+" => acc + rhs,
+            "*" => acc * rhs,
+            _ => panic!("Unknown operator: {}", op),
+        };
+    }
+
+    acc
+}
+
+fn parse_operand<I>(tokens: &mut I) -> usize
+where
+    I: Iterator<Item = String>,
+{
+    match tokens.next().as_deref() {
+        Some("(") => parse_expr(tokens),
+        Some(num) if num.chars().all(char::is_numeric) => num.parse().unwrap(),
+        Some(")") => panic!("Unexpected `)` when expecting operand"),
+        other => panic!("Unexpected operand: {:?}", other),
+    }
 }
 
 fn split_token(element: &str) -> Vec<&str> {
@@ -52,3 +88,4 @@ fn split_token(element: &str) -> Vec<&str> {
 
     tokens
 }
+
