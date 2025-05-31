4acff568699eaae633354939748352de8a8f5478
xi xiao <tdxiaoxi2@gmail.com>
2025-05-31 08:11:32 +0300

    +

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,34 @@
-use aoc_2020::day1::*;
-use aoc_2020::day2::*;
-use aoc_2020::day3::*;
-use aoc_2020::day4::*;
-use aoc_2020::day5::*;
-use aoc_2020::day6::*;
-use aoc_2020::day7::*;
-use aoc_2020::day8a::*;
-use aoc_2020::day8b::*;
-use aoc_2020::day9a::*;
-use aoc_2020::day9b::*;
-use aoc_2020::day10a::*;
-use aoc_2020::day10b::*;
-use aoc_2020::day11a::*;
-use aoc_2020::day11b::*;
-use aoc_2020::day12a::*;
-use aoc_2020::day12b::*;
-use aoc_2020::day13a::*;
-use aoc_2020::day13b::*;
-use aoc_2020::day14a::*;
-use aoc_2020::day14b::*;
-use aoc_2020::day15a::*;
-use aoc_2020::day15b::*;
-use aoc_2020::day16a::*;
-use aoc_2020::day16b::*;
-use aoc_2020::day17a::*;
-use aoc_2020::day17b::*;
+// use aoc_2020::day1::*;
+// use aoc_2020::day2::*;
+// use aoc_2020::day3::*;
+// use aoc_2020::day4::*;
+// use aoc_2020::day5::*;
+// use aoc_2020::day6::*;
+// use aoc_2020::day7::*;
+// use aoc_2020::day8a::*;
+// use aoc_2020::day8b::*;
+// use aoc_2020::day9a::*;
+// use aoc_2020::day9b::*;
+// use aoc_2020::day10a::*;
+// use aoc_2020::day10b::*;
+// use aoc_2020::day11a::*;
+// use aoc_2020::day11b::*;
+// use aoc_2020::day12a::*;
+// use aoc_2020::day12b::*;
+// use aoc_2020::day13a::*;
+// use aoc_2020::day13b::*;
+// use aoc_2020::day14a::*;
+// use aoc_2020::day14b::*;
+// use aoc_2020::day15a::*;
+// use aoc_2020::day15b::*;
+// use aoc_2020::day16a::*;
+// use aoc_2020::day16b::*;
+// use aoc_2020::day17a::*;
+// use aoc_2020::day17b::*;
+use aoc_2020::day18a::*;
 
 pub fn main() {
-    match solve_day17b() {
+    match solve_day18a() {
         Ok(count) => {
             println!("Result: {}", count);
         }
@@ -35,217 +36,4 @@ pub fn main() {
             eprintln!("Err: {}", e);
         }
     }
-    // match solve_day16a() {
-    //     Ok(count) => {
-    //         println!("Result: {}", count);
-    //     }
-    //     Err(e) => {
-    //         eprintln!("Err: {}", e);
-    //     }
-    // }
-    // match solve_day15a() {
-    //     Ok(count) => {
-    //         println!("Result: {}", count);
-    //     }
-    //     Err(e) => {
-    //         eprintln!("Err: {}", e);
-    //     }
-    // }
-    // match solve_day14a() {
-    //     Ok(count) => {
-    //         println!("Result: {}", count);
-    //     }
-    //     Err(e) => {
-    //         eprintln!("Err: {}", e);
-    //     }
-    // }
-    // match solve_day13b() {
-    //     Ok(count) => {
-    //         println!("Result: {}", count);
-    //     }
-    //     Err(e) => {
-    //         eprintln!("Err: {}", e);
-    //     }
-    // }
-    // match solve_day13a() {
-    //     Ok(count) => {
-    //         println!("Result: {}", count);
-    //     }
-    //     Err(e) => {
-    //         eprintln!("Err: {}", e);
-    //     }
-    // }
-    // match solve_day12b() {
-    //     Ok(count) => {
-    //         println!("Result: {}", count);
-    //     }
-    //     Err(e) => {
-    //         eprintln!("Err: {}", e);
-    //     }
-    // }
-    // match solve_day12a() {
-    //     Ok(count) => {
-    //         println!("Result: {}", count);
-    //     }
-    //     Err(e) => {
-    //         eprintln!("Err: {}", e);
-    //     }
-    // }
-    // match solve_day11b() {
-    //     Ok(count) => {
-    //         println!("Result: {}", count);
-    //     }
-    //     Err(e) => {
-    //         eprintln!("Err: {}", e);
-    //     }
-    // }
-    // match solve_day11a() {
-    //     Ok(count) => {
-    //         println!("Result: {}", count);
-    //     }
-    //     Err(e) => {
-    //         eprintln!("Err: {}", e);
-    //     }
-    // }
-    // match solve_day10b() {
-    //     Ok(count) => {
-    //         println!("Result: {}", count);
-    //     }
-    //     Err(e) => {
-    //         eprintln!("Err: {}", e);
-    //     }
-    // }
-    // match solve_day10a() {
-    //     Ok(count) => {
-    //         println!("Result: {}", count);
-    //     }
-    //     Err(e) => {
-    //         eprintln!("Err: {}", e);
-    //     }
-    // }
-    // match solve_day9b() {
-    //     Ok(count) => {
-    //         println!("Result: {}", count);
-    //     }
-    //     Err(e) => {
-    //         eprintln!("Err: {}", e);
-    //     }
-    // }
-    // match solve_day9a() {
-    //     Ok(count) => {
-    //         println!("Result: {}", count);
-    //     }
-    //     Err(e) => {
-    //         eprintln!("Err: {}", e);
-    //     }
-    // }
-    // match solve_day7b() {
-    //     Ok(count) => {
-    //         println!("Result: {}", count);
-    //     }
-    //     Err(e) => {
-    //         eprintln!("Err: {}", e);
-    //     }
-    // }
-    // match solve_day7a() {
-    //     Ok(count) => {
-    //         println!("Result: {}", count);
-    //     }
-    //     Err(e) => {
-    //         eprintln!("Err: {}", e);
-    //     }
-    // }
-    // match solve_day6b() {
-    //     Ok(count) => {
-    //         println!("Result: {}", count);
-    //     }
-    //     Err(e) => {
-    //         eprintln!("Err: {}", e);
-    //     }
-    // }
-    // match solve_day6a() {
-    //     Ok(count) => {
-    //         println!("Result: {}", count);
-    //     }
-    //     Err(e) => {
-    //         eprintln!("Err: {}", e);
-    //     }
-    // }
-    // match solve_day5b() {
-    //     Ok(count) => {
-    //         println!("Result: {}", count);
-    //     }
-    //     Err(e) => {
-    //         eprintln!("Err: {}", e);
-    //     }
-    // }
-    // match solve_day5a() {
-    //     Ok(count) => {
-    //         println!("Result: {}", count);
-    //     }
-    //     Err(e) => {
-    //         eprintln!("Err: {}", e);
-    //     }
-    // }
-    // match solve_day4b() {
-    //     Ok(count) => {
-    //         println!("Result: {}", count);
-    //     }
-    //     Err(e) => {
-    //         eprintln!("Err: {}", e);
-    //     }
-    // }
-    // match solve_day4a() {
-    //     Ok(count) => {
-    //         println!("Result: {}", count);
-    //     }
-    //     Err(e) => {
-    //         eprintln!("Err: {}", e);
-    //     }
-    // }
-    // match solve_day3b() {
-    //     Ok(count) => {
-    //         println!("Result: {}", count);
-    //     }
-    //     Err(e) => {
-    //         eprintln!("Err: {}", e);
-    //     }
-    // }
-    // match solve_day2b() {
-    //     Ok(count) => {
-    //         println!("Result: {}", count);
-    //     }
-    //     Err(e) => {
-    //         eprintln!("Err: {}", e);
-    //     }
-    // }
-    // match solve_day2a() {
-    //     Ok(count) => {
-    //         println!("Result: {}", count);
-    //     }
-    //     Err(e) => {
-    //         eprintln!("Err: {}", e);
-    //     }
-    // }
-    // match solve_day1b() {
-    //     Ok(Some(result)) => {
-    //         println!("Result: {}", result);
-    //     }
-    //     Ok(None) => {
-    //         eprintln!("No reuslt found!");
-    //     }
-    //     Err(e) => {
-    //         eprintln!("Err: {}", e);
-    //     }
-    // }
-    // match solve_day1a() {
-    //     Ok(Some(result)) => {
-    //         println!("Result: {}", result);
-    //     }
-    //     Ok(None) => {
-    //         eprintln!("No reuslt found!");
-    //     }
-    //     Err(e) => {
-    //         eprintln!("Err: {}", e);
-    //     }
 }
