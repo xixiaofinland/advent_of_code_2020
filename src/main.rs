ac2d2a21b6ea226c01d144aa54b334db004817e7
xi xiao <tdxiaoxi2@gmail.com>
2025-06-02 20:06:09 +0300

    +

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,3 @@
-// use aoc_2020::day1::*;
-// use aoc_2020::day2::*;
-// use aoc_2020::day3::*;
-// use aoc_2020::day4::*;
-// use aoc_2020::day5::*;
-// use aoc_2020::day6::*;
-// use aoc_2020::day7::*;
-// use aoc_2020::day8a::*;
-// use aoc_2020::day8b::*;
-// use aoc_2020::day9a::*;
-// use aoc_2020::day9b::*;
-// use aoc_2020::day10a::*;
-// use aoc_2020::day10b::*;
-// use aoc_2020::day11a::*;
-// use aoc_2020::day11b::*;
-// use aoc_2020::day12a::*;
-// use aoc_2020::day12b::*;
-// use aoc_2020::day13a::*;
-// use aoc_2020::day13b::*;
-// use aoc_2020::day14a::*;
-// use aoc_2020::day14b::*;
-// use aoc_2020::day15a::*;
-// use aoc_2020::day15b::*;
-// use aoc_2020::day16a::*;
-// use aoc_2020::day16b::*;
-// use aoc_2020::day17a::*;
-// use aoc_2020::day17b::*;
-// use aoc_2020::day18a::*;
-// use aoc_2020::day18b::*;
 use aoc_2020::day19a::*;
 
 pub fn main() {
