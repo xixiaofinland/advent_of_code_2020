a54cf86cb7920cc328681b4152a5718f653e963f
xi xiao <tdxiaoxi2@gmail.com>
2025-06-15 18:04:15 +0300

    +

--- src/day22.rs
+++ src/day22.rs
@@ -1,22 +1,55 @@
+use itertools::Itertools;
+
 use crate::AoCResult;
 use std::fs;
 
 pub fn solve_day22a() -> AoCResult<usize> {
     let file = fs::read_to_string("data/input_day22a_simple.txt")?;
-    let (player1, player2) = file
+    let (player1_data, player2_data) = file
         .split_once("\n\n")
         .ok_or_else(|| "cannot split_once correctly.")?;
-    let palyer1_stack: Vec<usize> = player1
+    let mut player1: Vec<usize> = player1_data
         .lines()
         .skip(1)
         .map(|n| n.parse::<usize>())
-        .collect::<Result<_,_>>()?;
+        .collect::<Result<_, _>>()?;
 
-    let palyer2_stack: Vec<usize> = player2
+    let mut player2: Vec<usize> = player2_data
         .lines()
         .skip(1)
         .map(|n| n.parse::<usize>())
-        .collect::<Result<_,_>>()?;
+        .collect::<Result<_, _>>()?;
 
-    Ok(0)
+    calculate(&mut player1, &mut player2);
+
+    let winner = if !player1.is_empty() {
+        player1
+    } else {
+        player2
+    };
+
+    let sum: usize = winner
+        .iter()
+        .rev()
+        .enumerate()
+        .map(|(i, &val)| (i + 1) * val)
+        .sum();
+
+    Ok(sum)
+}
+
+fn calculate(player1: &mut Vec<usize>, player2: &mut Vec<usize>) {
+    while !player1.is_empty() && !player2.is_empty() {
+        if player1[0] > player2[0] {
+            let first = player1.remove(0);
+            player1.push(first);
+            player1.push(player2.remove(0));
+        } else if player1[0] < player2[0] {
+            let first = player2.remove(0);
+            player2.push(first);
+            player2.push(player1.remove(0));
+        } else {
+            unreachable!()
+        }
+    }
 }
